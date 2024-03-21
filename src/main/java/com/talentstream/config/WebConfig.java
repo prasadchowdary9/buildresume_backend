968dea66d9384a26b50aba5234e613934bd4f87f
Nagul-Tekworks <161418040+Nagul-Tekworks@users.noreply.github.com>
2024-03-21 14:47:47 +0530

    Update WebConfig.java

--- src/main/java/com/talentstream/config/WebConfig.java
+++ src/main/java/com/talentstream/config/WebConfig.java
@@ -82,13 +82,15 @@ public class WebConfig {
     } */
     @Bean
     public FilterRegistrationBean corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        
+  UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);       
-       config.addAllowedOrigin("https://d3l89o84n3y7ig.cloudfront.net");
+        config.addAllowedOrigin("https://d2csdrjtu6a4ys.cloudfront.net");
         config.addAllowedOrigin("http://localhost:3000");       
-       config.addAllowedOrigin("https://talent-stream-front-end-version2-eedekarunakar.vercel.app");
-       config.addAllowedHeader("*");
+        config.addAllowedOrigin("https://talent-stream-front-end-version2-eedekarunakar.vercel.app");
+       
+         config.addAllowedOrigin("https://talentstream-deployment-bucket.s3.amazonaws.com");
          config.setAllowedHeaders(Arrays.asList(
                  HttpHeaders.AUTHORIZATION,
                  HttpHeaders.CONTENT_TYPE,
