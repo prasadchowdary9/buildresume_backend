bb14806773df878e70c25eee4c4364b9bf5bae99
suhelshaik113 <gentlemancadetsuhel@gmail.com>
2024-07-17 17:47:07 +0530

    Service

--- src/main/java/com/talentstream/service/EligibilityService.java
+++ src/main/java/com/talentstream/service/EligibilityService.java
@@ -10,14 +10,15 @@ import com.talentstream.repository.EligibilityRepository;
 public class EligibilityService {
     EligibilityRepository eligibilityRepository;
 
+    // Initializes the EligibilityService with the provided eligibility repository.
     @Autowired
     public EligibilityService(EligibilityRepository eligibilityRepository) {
         this.eligibilityRepository = eligibilityRepository;
     }
 
+    // Saves the provided eligibility record to the repository.
     public Eligibility saveEligibility(Eligibility eligibility) {
         return eligibilityRepository.save(eligibility);
     }
 
-    // Other methods if needed
 }
