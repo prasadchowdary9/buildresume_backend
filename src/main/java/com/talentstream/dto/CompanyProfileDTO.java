e4c71a9de755e3f37444c0523582d4266bbca95c
suhelshaik113 <gentlemancadetsuhel@gmail.com>
2024-07-17 16:14:18 +0530

    code-cleaning-dto

--- src/main/java/com/talentstream/dto/CompanyProfileDTO.java
+++ src/main/java/com/talentstream/dto/CompanyProfileDTO.java
@@ -2,7 +2,6 @@ package com.talentstream.dto;
 
 import java.util.List;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -10,70 +9,83 @@ import javax.validation.constraints.Size;
 public class CompanyProfileDTO {
 	private Long id;
 	@NotBlank(message = "Company name cannot be blank")
-    @Size(min = 3, message = "Company name must be at least 3 characters")
-    private String companyName;
+	@Size(min = 3, message = "Company name must be at least 3 characters")
+	private String companyName;
 	@NotBlank(message = "Website cannot be blank")
-    @Pattern(regexp = "^(.+)\\.(com|in|org)$", message = "Website must end with .com, .in, or .org")
-    private String website;
-	//@Pattern(regexp = "^\\d{10}$", message = "Phone number should be 10 digits")
-    private String phoneNumber;
-	//@Email(message = "Invalid email format")
-    private String email;
+	@Pattern(regexp = "^(.+)\\.(com|in|org)$", message = "Website must end with .com, .in, or .org")
+	private String website;
+	private String phoneNumber;
+	private String email;
 	@NotBlank(message = "Head office cannot be blank")
-    @Size(min = 3, message = "Head office address must be at least 3 characters")
-    private String headOffice;
- 
-    private byte[] logo;
-    
-    private List<String> socialProfiles;
+	@Size(min = 3, message = "Head office address must be at least 3 characters")
+	private String headOffice;
+
+	private byte[] logo;
+
+	private List<String> socialProfiles;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getCompanyName() {
 		return companyName;
 	}
+
 	public void setCompanyName(String companyName) {
 		this.companyName = companyName;
 	}
+
 	public String getWebsite() {
 		return website;
 	}
+
 	public void setWebsite(String website) {
 		this.website = website;
 	}
+
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
+
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getHeadOffice() {
 		return headOffice;
 	}
+
 	public void setHeadOffice(String headOffice) {
 		this.headOffice = headOffice;
 	}
+
 	public List<String> getSocialProfiles() {
 		return socialProfiles;
 	}
+
 	public void setSocialProfiles(List<String> socialProfiles) {
 		this.socialProfiles = socialProfiles;
 	}
+
 	public byte[] getLogo() {
 		return logo;
 	}
+
 	public void setLogo(byte[] logo) {
 		this.logo = logo;
 	}
-  	
-    
+
 }
--- src/main/java/com/talentstream/dto/LoginDTO.java
+++ src/main/java/com/talentstream/dto/LoginDTO.java
@@ -2,51 +2,49 @@ package com.talentstream.dto;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-
-import lombok.Data;
-
 
 public class LoginDTO {
-	
-	 @NotBlank(message = "Email is required.")
-	    @Pattern(
-	            regexp = "^$|^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$",
-	            message = "Invalid email format and white spaces are not allowed."
-	        )
-    private String email;
-	 
-    private String password;
 
-	private String status = "active"; // New field with a default value of "active"
-	
-	
-    private String utmSource;
+	@NotBlank(message = "Email is required.")
+	@Pattern(regexp = "^$|^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$", message = "Invalid email format and white spaces are not allowed.")
+	private String email;
 
-    public String getUtmSource() {
-	return utmSource;
-    }
-    public void setUtmSource(String utmSource) {
-	this.utmSource = utmSource;
-     }
-	    public String getStatus() {
-	        return status;
-	    }
- 
-	    public void setStatus(String status) {
-	        this.status = status;
-	    }
-    public String getEmail() {
+	private String password;
+
+	private String status = "active";
+
+	private String utmSource;
+
+	public String getUtmSource() {
+		return utmSource;
+	}
+
+	public void setUtmSource(String utmSource) {
+		this.utmSource = utmSource;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-    
+
 }
