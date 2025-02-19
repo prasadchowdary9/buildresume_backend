9661f95bb49df553f47327114d12947dded0be73
Nagul Meera Shaik <161418040+Nagul-Tekworks@users.noreply.github.com>
2025-02-19 16:13:16 +0530

    Update AwsSecretsManagerUtil.java

--- src/main/java/com/talentstream/AwsSecretsManagerUtil.java
+++ src/main/java/com/talentstream/AwsSecretsManagerUtil.java
@@ -1,5 +1,5 @@
 package com.talentstream;
-
+ 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
@@ -10,51 +10,30 @@ import software.amazon.awssdk.services.secretsmanager.model.SecretsManagerExcept
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-
+ 
 @Configuration
-
 public class AwsSecretsManagerUtil {
-
+ 
 	public static String getSecret() {
-		try {
-			String secrets = System.getenv("AWS_ACCESS_KEY_ID");
-			System.out.println("Secretes  " + secrets);
-			JSONObject jsonObject = new JSONObject(secrets);
-			String accessKey = jsonObject.getString("AWS_ACCESS_KEY_ID");
-			String secretKey = jsonObject.getString("AWS_SECRET_ACCESS_KEY");
-			String region1 = jsonObject.getString("AWS_REGION");
-			Region region = Region.of(region1);
+		  try {
+		  String secrets = System.getenv("AWS_ACCESS_KEY_ID");
+		  System.out.println("Secretes  "+secrets);
+		  JSONObject jsonObject = new JSONObject(secrets);
+	        String accessKey = jsonObject.getString("AWS_ACCESS_KEY_ID");
+	        String secretKey = jsonObject.getString("AWS_SECRET_ACCESS_KEY");
+	        String region1 = jsonObject.getString("AWS_REGION");
+	        Region region = Region.of(region1);
 
-			if (accessKey == null || secretKey == null) {
-				System.err.println("AWS credentials are not set in environment variables.");
-				return null;
-			}
-
-			return secrets;
-		} catch (Exception e) {
-			System.err.println("An error occurred: " + e.getMessage());
-			return null;
-		}
-	}
-
-	public String getDbUsername() {
-		String secret = getSecret();
-		if (secret == null) {
-			return null;
-		}
-
-		JSONObject jsonObject = new JSONObject(secret);
-		return jsonObject.getString("DB_USERNAME");
-	}
-
-	public String getDbPassword() {
-		String secret = getSecret();
-		if (secret == null) {
-			return null;
-		}
-
-		JSONObject jsonObject = new JSONObject(secret);
-		return jsonObject.getString("DB_PASSWORD");
-	}
-
+	        if (accessKey == null || secretKey == null) {
+	            System.err.println("AWS credentials are not set in environment variables.");
+	            return null;
+	        }
+	            return secrets;
+	        } catch (Exception e) {
+	            System.err.println("An error occurred: " + e.getMessage());
+	            return null;
+	        }
+    }
+ 
+    
 }
